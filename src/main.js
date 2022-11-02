4725404fd254c7462ccfd1b7338756d3f3508d3c
VWongWorkingAtBask <82612897+vw-bask@users.noreply.github.com>
2022-11-02 10:49:30 -0700

    added vite-plugin-pages for Nuxt like routing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
-import { createWebHistory } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'; 
 
-import createRouter from './pages/routes.js'
+//import createRouter from './pages/routes.js'
+//import createRouter from './lib/router.js'
 import App from './App.vue'
 
-const router = createRouter(createWebHistory())
+//const router = createRouter(createWebHistory())
+const router = createRouter({
+  history: createWebHistory(),
+  routes: generatedRoutes,
+});
 const app = createApp(App)
 app.use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Pages({
+        pagesDir: [
+            {dir: 'src/pages', baseRoute: ''},
+        ],
+        extensions: ['vue'],
+        syncIndex: true,
+        replaceSquareBrackets: true,
+        extendRoute(route) {
+            if (route.name === 'about')
+                route.props = route => ({query: route.query.q})
+
+            if (route.name === 'components') {
+                return {
+                    ...route,
+                    beforeEnter: (route) => {
+                        // eslint-disable-next-line no-console
+                        // console.log(route)
+                    },
+                }
+            }
+        },
+    }), 
+  ]
 })
