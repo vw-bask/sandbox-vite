31f4040460036a9d6e1c0a5ff3d101ca19b00023
VWongWorkingAtBask <82612897+vw-bask@users.noreply.github.com>
2022-11-03 12:03:04 -0700

    added admin link

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,5 +1,6 @@
 <template>
     <router-link to="/">Home</router-link>
+    <router-link to="/admin/">Admin</router-link>
     <router-link to="/campaign-reports/">Campaign Reports</router-link>
     <router-link to="/projects/">Projects</router-link>
 </template>
